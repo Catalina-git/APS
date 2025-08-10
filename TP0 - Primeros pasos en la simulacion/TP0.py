8c9488e5e3c0c7c7e41d441924444c07e59d582b
Catalina <catagonzalezaraujo@gmail.com>
2025-08-10 11:09:45 -0300

    TP

--- TP0 - Primeros pasos en la simulacion/TP0.py
+++ TP0 - Primeros pasos en la simulacion/TP0.py
@@ -1,7 +1,42 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Aug  9 16:31:28 2025
 
-@author: Usuario
-"""
+# Primero importo la libreria numpy
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Defino mi funcion
+def mi_funcion_sen(amplitud = 1, offset = 0, frecuencia = 1, fase = 0, N = 1000, frecADC = 1000):
+    """
+    - amplitud: es la amplitud maxima. [amplitud] = [V]
+    - offset: es mi amplitud media. [offset] = [V]
+    - frecuencia: es la frecuencia de la señal. [frecuencia] = [Hz]
+    - fase: es la fase inicial. [fase] = [rad]
+    - N: es la cantidad de muestras a generar
+    - frecADC: es la frecuencia de muestreo del ADC. [frecADC] = [Hz]
+    """
+    Ts = 1/frecADC # Es el tiempo en el cual se toma cada muestra
+
+    tt = np.arange(start = 0, stop= N*Ts, step = Ts)
+
+    xx = amplitud * np.sin(2 * np.pi * frecuencia * tt + fase) + offset
+
+    return tt, xx
+
+
+# Defino mis variables
+amplitud = 2
+offset = 1
+frecuencia = 5
+fase = np.pi/4
+N = 1000
+frecADC = 1000
+
+# Llamo a mi funcion
+tt, xx = mi_funcion_sen(amplitud, offset, frecuencia, fase, N, frecADC)
+
+# Grafico la señal
+plt.title("Señal Senoidal Generada")
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud [V]')
+plt.plot(tt, xx, linestyle = '-', color = 'r' ) # Genero el grafico de la señal con linea 'continua' de color 'rojo'
 
