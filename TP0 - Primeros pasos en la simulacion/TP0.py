4113f4198a9072aba7662d9bbd6c66882de7d678
Catalina <catagonzalezaraujo@gmail.com>
2025-08-11 16:46:44 -0300

    Ejercicio Bonus - TS0

--- TP0 - Primeros pasos en la simulacion/TP0.py
+++ TP0 - Primeros pasos en la simulacion/TP0.py
@@ -40,3 +40,52 @@ plt.xlabel('Tiempo [s]')
 plt.ylabel('Amplitud [V]')
 plt.plot(tt, xx, linestyle = '-', color = 'r' ) # Genero el grafico de la señal con linea 'continua' de color 'rojo'
 
+# EJERCICIO BONUS (SIGUIENDO EL TEOREMA DE NYQUIST-SHANNON)
+
+# Genero otra ventana para los graficos
+plt.figure(figsize=(10, 6))  # Tamaño de la figura (ancho, alto)
+
+# Señal 500 Hz
+plt.subplot(2, 2, 1)
+tt, xx = mi_funcion_sen(2, 0, 500, np.pi/4, 1000, 100000)
+plt.plot(tt, xx, '-', color='blue')
+plt.title("Señal Senoidal con 500 Hz")
+plt.xlabel("Tiempo [s]")
+plt.ylabel("Amplitud [V]")
+plt.grid(True)
+
+# Señal 999 Hz
+plt.subplot(2, 2, 2)
+tt, xx = mi_funcion_sen(2, 0, 999, np.pi/4, 1000, 2000)
+plt.plot(tt, xx, '-', color='green')
+plt.title("Señal Senoidal con 999 Hz")
+plt.xlabel("Tiempo [s]")
+plt.ylabel("Amplitud [V]")
+plt.grid(True)
+
+# Señal 1001 Hz
+plt.subplot(2, 2, 3)
+tt, xx = mi_funcion_sen(2, 0, 1001, np.pi/4, 1000, 3000)
+plt.plot(tt, xx, '-', color='orange')
+plt.title("Señal Senoidal con 1001 Hz")
+plt.xlabel("Tiempo [s]")
+plt.ylabel("Amplitud [V]")
+plt.grid(True)
+
+# Señal 2001 Hz
+plt.subplot(2, 2, 4)
+tt, xx = mi_funcion_sen(2, 0, 2001, np.pi/4, 1000, 5000)
+plt.plot(tt, xx, '-', color='grey')
+plt.title("Señal Senoidal con 2001 Hz")
+plt.xlabel("Tiempo [s]")
+plt.ylabel("Amplitud [V]")
+plt.grid(True)
+
+plt.tight_layout()  
+plt.show()
+
+
+
+
+
+
